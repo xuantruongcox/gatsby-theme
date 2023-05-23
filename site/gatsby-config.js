e7a3cb9575abeaf450157887dcfc15e74dce6bcd
xuantruongcox <truongcool999@gmail.com>
2023-05-23 18:18:09 +0700

    DONE

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -1,11 +1,8 @@
 module.exports = {
-    plugins: [
-        {
-            resolve: "gatsby-theme-events",
-            options: {
-                contentPath: "events",
-                basePath: '/events'
-            }
-        }
-    ]
-}
+  plugins: [
+    {
+      resolve: "gatsby-theme-events",
+      options: {},
+    },
+  ],
+};
